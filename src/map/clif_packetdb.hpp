5cdd63c79bd9737e4fb74bb6ea80cd8b5504edc5
richard <>
2026-01-09 22:03:35 +0900

    Implemented new stylist packet(2023-12-18)

--- src/map/clif_packetdb.hpp
+++ src/map/clif_packetdb.hpp
@@ -1921,7 +1921,9 @@
 	packet(0x0ADD, 22);
 #endif
 
-#if PACKETVER >= 20180516
+#if PACKETVER >= 20231218
+	parseable_packet( HEADER_CZ_REQ_STYLE_CHANGE3, sizeof( PACKET_CZ_REQ_STYLE_CHANGE3 ), clif_parse_stylist_buy, 0 );
+#elif PACKETVER >= 20180516
 	parseable_packet( HEADER_CZ_REQ_STYLE_CHANGE2, sizeof( PACKET_CZ_REQ_STYLE_CHANGE2 ), clif_parse_stylist_buy, 0 );
 #endif
 
